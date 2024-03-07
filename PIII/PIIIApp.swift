98dcbaae0c38bf079ff2df145149b30a2927bb94
Space Inc <103204054+Spaceraf@users.noreply.github.com>
2024-03-07 22:23:32 +0000

    Initial Commit

---
+++ PIII/PIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIIIApp.swift
+//  PIII
+//
+//  Created by builder on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
